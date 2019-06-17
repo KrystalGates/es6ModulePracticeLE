7ff624cb133a2e3bc7056cc1a33570ac7d1b6e20
krystal gates <krystal_sabrina@yahoo.com>
2019-06-17 13:06:48 -0500

    completed es6-module exercise) :x
    :x

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,10 @@
 // 3) Create add and subtract functions in their respective modules that act on a number passed into them and the private var
 // 4) Import both modules into main.js
 // 5) execute both function in main.js
+
+import {subtract} from "./subtract.js"
+import {add} from "./add.js"
+
+console.log(subtract(6))
+
+console.log(add(4))
